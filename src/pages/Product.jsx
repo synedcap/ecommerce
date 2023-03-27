c66180b0acb90a1a0e1bbffaaf57b8b9f01b7db6
Sassus Capelo <synedcap@gmail.com>
2023-03-27 17:53:18 +0100

    Initial commit

---
+++ src/pages/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = (props) => {
+    const {id,productName,price,productImage} = props.data
+    return (
+        <div>
+            Product
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/products.js
@@ -0,0 +1,52 @@
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Camera Nikon",
+    price: 1999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Sac Lv",
+    price: 2050.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Iphone 13 bleu",
+    price: 999.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Sneakers",
+    price: 650.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Iphone 12 pro",
+    price: 899.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Gadget",
+    price: 3800.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Iphone 11 Rouge",
+    price: 750.0,
+    productImage: product7,
+  },
+];
