7e0114d4aca2d69e3efd5ae5a617224eefc44051
Sassus Capelo <synedcap@gmail.com>
2023-03-28 16:11:51 +0100

    display subtotal

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -15,16 +15,40 @@ const getDefaultCart = () => {
 const ShopContext = (props) => {   
 
     const [ cartItems, setCartItems] = useState(getDefaultCart());
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in  cartItems){
+            if (cartItems[item] > 0) {
+                let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+                totalAmount += cartItems[item] * itemInfo.price
+            }
+
+        }
+
+        return totalAmount;
+    }
+
     const addToCart = (itemId) => {
         setCartItems((prev) =>({...prev,[itemId]: prev[itemId] + 1}))
     }
+
     const removeFromCart = (itemId) => {
         setCartItems((prev) =>({...prev,[itemId]: prev[itemId] - 1}))
     }
+
     const updateCartItemCount = (newAmount,itemId) => {
         setCartItems((prev) =>({...prev,[itemId]: newAmount}))
     }
-    const contextvalue = { cartItems, addToCart, removeFromCart,updateCartItemCount}
+
+    const contextvalue = { 
+        cartItems, 
+        addToCart, 
+        removeFromCart,
+        updateCartItemCount,
+        getTotalCartAmount
+
+    }
 
     return (
         <SpContext.Provider value={contextvalue}>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,9 +4,13 @@ import { PRODUCTS } from "../products";
 
 import "../styles/Cart.css";
 import CartItem from "./CartItem";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const { cartItems } = useContext(SpContext);
+  const { cartItems,getTotalCartAmount } = useContext(SpContext);
+  const totalAmount =  getTotalCartAmount()
+
+  const navigate =  useNavigate();
   return (
     <div className="cart">
       <div>
@@ -19,6 +23,15 @@ const Cart = () => {
           }
         })}
       </div>
+      {totalAmount > 0 ?(
+      <div className="checkout">
+        <p>Subtotal: ${totalAmount}</p>
+        <button onClick = {() => navigate("/")}>Continue Shopping</button>
+        <button>Checkout</button>
+      </div>
+      ) : (
+        <h1>Your Cart is Empty</h1>
+      )}
     </div>
   );
 };
